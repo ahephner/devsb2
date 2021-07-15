9a0a6921316d58487f73623c9c0ffa12bc254750
ahephner <ajhephner178@gmail.com>
2021-07-15 14:26:36 -0400

    dont love it but ready to try as another platform

--- force-app/main/default/lwc/userProvision/userProvision.js
+++ force-app/main/default/lwc/userProvision/userProvision.js
@@ -1,6 +1,8 @@
 import { LightningElement, api, wire } from 'lwc';
 import getContact from '@salesforce/apex/communityProvisionUser.getContact'; 
- import USER_OBJ from '@salesforce/schema/User';
+import insertUser from '@salesforce/apex/communityProvisionUser.insertUser';
+import { FlowNavigationBackEvent, FlowNavigationNextEvent } from 'lightning/flowSupport';
+//import USER_OBJ from '@salesforce/schema/User';
 // import FIRST_NAME from '@salesforce/schema/User.FirstName';
 // import LAST_NAME from '@salesforce/schema/User.LastName';
 // import EMAIL from '@salesforce/schema/User.Email';
@@ -20,7 +22,7 @@ export default class UserProvision extends LightningElement {
     @api profileName
     error
     contact; 
-    userObj = USER_OBJ; 
+    //userObj = USER_OBJ; 
     firstName 
     lastName
     email;
@@ -43,22 +45,83 @@ export default class UserProvision extends LightningElement {
         wiredContact({data, error}){
             if(data){
                this.contact = data
+               this.copy = this.contact; 
                //console.log(this.contact);
-                 this.firstName = this.contact.FirstName;
-                 this.lastName = this.contact.LastName;
-                 this.email = this.contact.Email;
-                 this.userName = this.contact.Email; 
-                 this.company = this.contact.Company_Name__c;
-                 this.phone = this.contact.Phone; 
-                 this.alias = this.contact.FirstName.substring(0,1) + this.contact.LastName.substring(0,8);
+                 this.firstName = this.copy.FirstName;
+                 this.lastName = this.copy.LastName;
+                 this.email = this.copy.Email;
+                 this.userName = this.copy.Email; 
+                 this.company = this.copy.Company_Name__c;
+                 this.phone = this.copy.Phone; 
+                 this.alias = this.copy.FirstName.substring(0,1) + this.copy.LastName.substring(0,8);
                  this.nickName = this.firstName +" "+ this.lastName
                 console.log(this.firstName)
-               console.log(typeof this.contact.LastName)
+               console.log(typeof this.copy.LastName)
             }else if(error){
                 this.error = JSON.stringify(error);
                 console.log(error); 
             }
     }  
 
+    handleFName(e){
+        this.firstName = e.detail.value;       
+    }
+    handleLName(e){
+        this.lastName = e.detail.value;
+    }
+    handleCompany(e){
+        this.company = e.detail.value;
+    }
+    handleEmail(e){
+        this.email = e.detail.value;
+    }
+    handleUsername(e){
+        this.userName = e.detail.value;
+    } 
+    handlePhone(e){
+        this.phone = e.detail.value;
+    }
+    handleAlias(e){
+        this.alias = e.detail.value;
+    }
+    handleNickname(e){
+        this.nickName = e.detail.value;
+    }
 
+    save(){
+        let params ={
+            fName : this.firstName,
+            lName: this.lastName,
+            email: this.email,
+            uName : this.userName,
+            company: this.company,
+            phone: this.phone,
+            alias: this.alias,
+            nickname: this.nickName,
+            profileId : this.profileId,
+            contactId: this.contactId
+        }
+        insertUser({wrapper:params})
+            .then((resp)=>{
+                console.log('rep '+resp);
+                const next = FlowNavigationNextEvent();
+                this.dispatchEvent(next); 
+            }).catch((error)=>{
+                console.log(JSON.stringify(error))
+                let message = 'Unknown error';
+                if (Array.isArray(error.body)) {
+                    message = error.body.map(e => e.message).join(', ');
+                    this.errorMessage = message; 
+                } else if (typeof error.body.message === 'string') {
+                    message = error.body.message;
+                    this.errorMessage = message; 
+                }
+         })
+        }
+
+        back(){
+            const backNav = FlowNavigationBackEvent();
+            this.dispatchEvent(backNav);
+        }
+    
 }
