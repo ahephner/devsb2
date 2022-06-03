e006e0e0feeb89d45e325fb5968afcac95a11b08
ahephner <ajhephner178@gmail.com>
2022-06-03 16:36:22 -0400

    for Tracie

---
+++ force-app/main/default/lwc/bg_B2C_ContactUs/bg_B2C_ContactUs.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+// import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+// import STATUS_FIELD from '@salesforce/schema/Case.Status';
+// import ORIGIN_FIELD from '@salesforce/schema/Case.Origin';
+// import SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
+
+export default class Bg_B2C_ContactUs extends LightningElement {
+    // @api objectApiName = 'Case';
+    // fields = [STATUS_FIELD, ORIGIN_FIELD, SUBJECT_FIELD];
+    // handleSuccess(event) {
+    //     const evt = new ShowToastEvent({
+    //         title: 'Case created',
+    //         message: 'Record ID: ' + event.detail.id,
+    //         variant: 'success',
+    //     });
+    //     this.dispatchEvent(evt);
+    // }
+}
